89e19e7b70bdb7cd556507d8dcdca38781534a3c
Jefferson Rago <jefferson.s.rago@gmail.com>
2018-02-11 14:03:11 -0200

    Adicionando estrtutura do projeto

---
+++ src/main/java/br/com/jeffersonrago/alura/java8/AluraJava8Application.java
@@ -0,0 +1,12 @@
+package br.com.jeffersonrago.alura.java8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraJava8Application.class, args);
+	}
+}
